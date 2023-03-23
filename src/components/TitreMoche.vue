0a741e454c271739806071d5154114df61edd203
Emestay <103575914+Emestay@users.noreply.github.com>
2023-03-23 09:23:32 +0100

    titremoche

---
+++ src/components/TitreMoche.vue
@@ -0,0 +1,43 @@
+<template>
+    <h1 @click="reverseTitle" class="ugly-title">{{ title }}</h1>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    setup() {
+      const title = ref('Docteur Quinn, Femme médecin !');
+  
+      function reverseTitle() {
+        title.value = title.value.split('').reverse().join('');
+      }
+  
+      return {
+        title,
+        reverseTitle,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .ugly-title {
+    font-family: 'Comic Sans MS', sans-serif;
+    font-size: 48px;
+    text-align: center;
+    color: #ff00ff;
+    text-shadow: 3px 3px 0px #0000ff, 6px 6px 0px rgba(0, 0, 255, 0.4);
+  }
+  
+  .ugly-title:hover {
+    text-shadow: 3px 3px 0px #0000ff, -3px -3px 0px rgba(0, 0, 255, 0.4);
+    cursor: pointer;
+    transition: all 0.3s ease;
+  }
+  .ugly-title::selection{
+    background-color: transparent;
+  color: inherit;
+  }
+  </style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <script setup>
 import Carroussel from '../components/Carroussel.vue';
+import TitreMoche from '../components/TitreMoche.vue';
 
 
 </script>
 
 <template>
+  <br>
+  <TitreMoche></TitreMoche>
+  <br>
   <main>
     <Carroussel></Carroussel>
   </main>
